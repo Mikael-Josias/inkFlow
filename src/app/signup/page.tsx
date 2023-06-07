99ab1834e2b025e76ee5640a761630775a0a4459
Mikael Josias <mikaeljosias2002@gmail.com>
2023-06-07 12:38:12 -0300

    refactor: signup page
    - use signup hook to call api
    - show toast with api response
    - validate form inputs
    - show input validation errors

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,67 @@
+'use client'
+import { ToastContainer, toast } from "react-toastify";
+import { useSignUp } from "@/hooks/api/useSignUp";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import Sign from "@/layouts/sign";
+import Link from "next/link";
+import { useEffect } from "react";
+
+export default function SignUp() {
+  const router = useRouter();
+  const { register, handleSubmit, formState: { errors }} = useForm();
+  const { signUpAct } = useSignUp();
+
+  console.log(errors)
+  const submit = async (data: any) => {
+    const {
+      name,
+      email,
+      password,
+      verifyPassword,
+    } = data;
+    
+    if (password !== verifyPassword) {
+      toast('As senhas são diferentes!');
+      return;
+    }
+
+    try {
+      await signUpAct({name, email, password});
+      router.push('/signin');
+      toast('Cadastro realizado com sucesso!');
+    } catch (err) {
+      toast('Erro ao realizar o cadastro!');
+    }
+  }
+
+  return (
+    <Sign title="Primeira vez?">
+      <form onSubmit={handleSubmit(submit)} className="flex flex-col gap-10 w-full mt-10 mb-1">
+        <label className="relative">
+          <input {...register("name", {required: true})} type="text" placeholder="Nome" className="w-[400px] h-[30px] text-white-600 border-white-600 border-b-[1px] outline-none"/>
+          { errors?.name && <span className="text-sm text-red-500 absolute bottom-[-25px] left-0">*Nome é obrigatório</span>}
+        </label>
+        <label className="relative">
+          <input {...register("email", {required: true})} type="email" placeholder="Email" className="w-[400px] h-[30px] text-white-600 border-white-600 border-b-[1px] outline-none"/>
+          { errors?.email && <span className="text-sm text-red-500 absolute bottom-[-25px] left-0">*Email é obrigatório</span>}
+        </label>
+        <label className="relative">
+          <input {...register("password", {required: true, minLength: 8})} type="password" placeholder="Senha"  className="w-[400px] h-[30px] text-white-600 border-white-600 border-b-[1px] outline-none"/>
+          { errors?.password?.type === "required" && <span className="text-sm text-red-500 absolute bottom-[-25px] left-0">*Senha é obrigatório</span>}
+          { errors?.password?.type === "minLength" && <span className="text-sm text-red-500 absolute bottom-[-25px] left-0">*Senha deve ter pelo menos 8 caracteres</span>}
+        </label>
+        <label className="relative">
+          <input {...register("verifyPassword", {required: true})} type="password" placeholder="Verifique a senha" className="w-[400px] h-[30px] text-white-600 border-white-600 border-b-[1px] outline-none"/>
+          { errors?.verifyPassword && <span className="text-sm text-red-500 absolute bottom-[-25px] left-0">*Verificar a senha é obrigatório</span>}
+        </label>
+
+        <input type="submit" value="CADASTRAR" className="h-8 bg-blue-500 mt-10 text-white font-bold rounded-md cursor-pointer hover:bg-blue-500/90 transition-colors" />
+      </form>
+      <div className="w-full text-center ">
+        <span className="text-white-600">Já possui uma conta? <Link href="/signin" className="text-blue-500 cursor-pointer">faça login!</Link></span>
+      </div>
+      <ToastContainer/>
+    </Sign>
+  )
+}
