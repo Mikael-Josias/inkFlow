144a3cb8439efcdb8328f60ed83af32a90cdce29
Mikael Josias <mikaeljosias2002@gmail.com>
2023-06-24 22:42:12 -0300

    refactor: document type array

--- src/hooks/api/useDocuments.ts
+++ src/hooks/api/useDocuments.ts
@@ -1,11 +1,11 @@
 import { documentsAPI } from "@/services/documentService";
 import { useAsync } from "../useAsync";
 
-export const useDocuments = () => {
+export const useDocuments = <T>() => {
   const {
     data: documentsData,
     loading: documentsLoading,
-  } = useAsync(documentsAPI, true);
+  } = useAsync<T>(documentsAPI, true);
 
   return {
     documentsData,
